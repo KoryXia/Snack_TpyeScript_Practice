f0c119440788c62c4c2aee8a437469c52bdc48ac
KoryXia <koryxia@icloud.com>
2023-01-04 22:34:51 +0100

    TS practice

--- src/Modules/ScorePanel.ts
+++ src/Modules/ScorePanel.ts
@@ -2,13 +2,9 @@ import {ref} from 'vue';
 class ScorePanel {
     static score = ref(0);
     static level = ref(1);
-    // scoreElement: HTMLElement;
-    // levelElement: HTMLElement;
     maxLevel: number;
     levelUpScore: number;
     constructor(maxLevel: number = 10, levelUpScore: number = 5) {
-        // this.scoreElement = document.getElementById("score")!;
-        // this.levelElement = document.getElementById("level")!;
         this.maxLevel = maxLevel;
         this.levelUpScore = levelUpScore;
     }
